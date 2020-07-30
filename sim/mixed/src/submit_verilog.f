2454346446283219446837c3d6eaac076ad14aa1
PacoReinaCampo <frareicam@gmail.com>
2020-07-30 03:26:55 +0200

    new commit

--- sim/mixed/src/submit_verilog.f
+++ sim/mixed/src/submit_verilog.f
@@ -36,7 +36,7 @@
  *
  * =============================================================================
  * Author(s):
- *   Francisco Javier Reina Campo <frareicam@gmail.com>
+ *   Paco Reina Campo <pacoreinacampo@queenfield.tech>
  */
 
 +incdir+../../../rtl/verilog/pkg/
