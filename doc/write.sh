b634282de5f315f9ca96a8475cc6816b91bc03d2
PacoReinaCampo <frareicam@gmail.com>
2022-08-11 10:38:04 +0200

    Working in PU-MSP430 🔨

--- doc/write.sh
+++ doc/write.sh
@@ -1,8 +1,5 @@
 rm -f *.tex
 rm -f *.pdf
 
-pandoc ../README.md -s -o PU-MSP430.tex
-pandoc ../README.md -s -o PU-MSP430.pdf
-
-pandoc ../README-ES.md -s -o PU-MSP430-ES.tex
-pandoc ../README-ES.md -s -o PU-MSP430-ES.pdf
+pandoc BOOK.md -s -o PU-MSP430.tex
+pandoc BOOK.md -s -o PU-MSP430.pdf
