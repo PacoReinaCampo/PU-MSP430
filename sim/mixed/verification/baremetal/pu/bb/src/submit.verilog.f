0c076e129f68a35cdce046154ec4336312146fa8
PacoReinaCampo <engineer@queenfield.tech>
2023-03-31 18:25:38 +0200

    Working in PU-MSP430 🔨

---
+++ sim/mixed/verification/baremetal/pu/bb/src/submit.verilog.f
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                            __ _      _     _               //
+//                                           / _(_)    | |   | |              //
+//                __ _ _   _  ___  ___ _ __ | |_ _  ___| | __| |              //
+//               / _` | | | |/ _ \/ _ \ '_ \|  _| |/ _ \ |/ _` |              //
+//              | (_| | |_| |  __/  __/ | | | | | |  __/ | (_| |              //
+//               \__, |\__,_|\___|\___|_| |_|_| |_|\___|_|\__,_|              //
+//                  | |                                                       //
+//                  |_|                                                       //
+//                                                                            //
+//                                                                            //
+//              MSP430 CPU                                                    //
+//              Processing Unit                                               //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+/* Copyright (c) 2015-2016 by the author(s)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * =============================================================================
+ * Author(s):
+ *   Paco Reina Campo <pacoreinacampo@queenfield.tech>
+ */
+
++incdir+../../../../../../../rtl/verilog/pkg/
+
+//=============================================================================
+// Testbench related
+//=============================================================================
+
+../../../../../../../bench/verilog/baremetal/main/ram_d1.sv
+../../../../../../../bench/verilog/baremetal/main/ram_d2.sv
+../../../../../../../bench/verilog/baremetal/main/ram_dp.sv
+../../../../../../../bench/verilog/baremetal/main/ram_p2.sv
+../../../../../../../bench/verilog/baremetal/main/ram_sp.sv
+../../../../../../../bench/verilog/baremetal/main/glbl.sv
+../../../../../../../bench/verilog/baremetal/main/msp430_debug.sv
+../../../../../../../bench/verilog/baremetal/main/msp430_testbench.sv
+
+
+//=============================================================================
+// SoC-MSP430
+//=============================================================================
+
+
+//=============================================================================
+// PU-MSP430
+//=============================================================================
---
+++ software/baremetal/leds/stimulus.sv
@@ -0,0 +1,1 @@
+../../../../../../../bench/verilog/baremetal/cases/leds.sv
